03fc18c29587dae1d4ad0b5b98ec3610a4b7324a
Hendri Alqori <teamhendri18@gmail.com>
2024-11-06 20:56:41 +0700

    feat: add constant value mock response

--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -1,6 +1,13 @@
-export const SERVER = process.env.NEXT_PUBLIC_SERVER
+export const SERVER = import.meta.env.VITE_SERVER;
 export const API = SERVER + "/api/v1"
-export const STATIC = SERVER + "/uploads"
+export const STATIC = SERVER + "/upload"
 
 export const TOKEN = "token"
+
+export const VALIDATION_ERROR = "VALIDATION_ERROR"
+export const RESPONSE_ERROR = "RESPONSE_ERROR"
+export const UNKNOWN_ERROR = "UNKNOWN_ERROR"
+export const AUTH_ERROR = "AUTH_ERROR"
+export const FILE_UPLOAD_ERROR = "FILE_UPLOAD_ERROR"
+
 
