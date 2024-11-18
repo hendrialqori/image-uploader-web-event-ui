fc5ef8e269c37b5f1ea8d2ba30e968ae617c514d
Hendri Alqori <teamhendri18@gmail.com>
2024-11-18 16:30:23 +0700

    feat: change background rest placement

--- src/pages/[admin] leaderboard/modules/rest-placement.tsx
+++ src/pages/[admin] leaderboard/modules/rest-placement.tsx
@@ -9,7 +9,7 @@ export default function RestPlacement({ data }: Props) {
     return (
         <div className="p-2 md:p-9" role="table" aria-label="table-placement">
             {data.map((placement, i) => (
-                <div key={i} className="grid grid-cols-5 text-pertamina-blue bg-transparent py-3 border-b border-gray-200 text-[0.65rem] md:text-base" role="row" aria-label="row">
+                <div key={i} className="grid grid-cols-5 text-pertamina-blue bg-[#F8F8F8] py-3 border-b border-gray-200 text-[0.65rem] md:text-base" role="row" aria-label="row">
                     <p className="px-3">{placement.rank}</p>
                     <p className={cn(placement.is_suspend ? "text-red-400" : "text-pertamina-blue", "col-span-2")}>{placement.username ?? "[unknown]"}</p>
                     <p>{placement.point ?? 0}pts</p>
